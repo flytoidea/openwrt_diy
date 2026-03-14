823e0b3690c73e396ab4273e139e7c5ded1d20cc
flytoidea <60851051+flytoidea@users.noreply.github.com>
2026-03-14 13:57:51 +0800

    Update mkbootscr.sh

--- files/rk3568/pro-rk3568-b/mkbootscr.sh
+++ files/rk3568/pro-rk3568-b/mkbootscr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3568/h68ktv/boot.cmd ../../bootfiles/rockchip/rk3568/h68ktv/boot.scr
+/usr/bin/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3568/pro-rk3568-b/boot.cmd ../../bootfiles/rockchip/rk3568/pro-rk3568-b/boot.scr
